ce13a0a03070a36ba4ca25a55d9c59aa32f1d237
Denys Pozniak <denys.pozniak@gmail.com>
2026-01-30 23:37:29 +0100

    Add no-tls-id flag to disable a=tls-id SDP attribute generation

--- daemon/call_interfaces.c
+++ daemon/call_interfaces.c
@@ -1255,6 +1255,12 @@ void call_ng_flags_flags(str *s, unsigned int idx, helper_arg arg) {
 		case CSH_LOOKUP("no RTCP attribute"):
 			out->no_rtcp_attr = true;
 			break;
+		case CSH_LOOKUP("no-tls-id"):
+		case CSH_LOOKUP("no tls-id"):
+		case CSH_LOOKUP("no-TLS-ID"):
+		case CSH_LOOKUP("no TLS-ID"):
+			out->no_tls_id = true;
+			break;
 		case CSH_LOOKUP("no-jitter-buffer"):
 		case CSH_LOOKUP("no jitter buffer"):
 			out->disable_jb = true;
